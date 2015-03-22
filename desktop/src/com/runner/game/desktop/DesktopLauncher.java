ef88ee07208144ddd6f2c706961f573d561dbec0
CullyCross <elaugfein@gmail.com>
2015-03-22 12:42:24 +0200

    first commit

---
+++ desktop/src/com/runner/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.runner.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.runner.game.RunnerGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunnerGame(), config);
+	}
+}
